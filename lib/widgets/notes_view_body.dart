846909c1ea81a335af56528505f72512188356ab
Mohamed Abdelmaboud <mohamedabdelmabod03@gmail.com>
2023-11-13 02:18:59 +0200

    .

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
-import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text.dart';
 import 'package:notes_app/widgets/no_notes_body.dart';
